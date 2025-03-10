a9a2ab075ddd07c66af038e822ffc699884977b3
Akram Hossain <nayanbd17@gmail.com>
2025-03-10 12:31:34 +0600

    details page done 2

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  root: './', // Ensure Vite looks in the root directory
   build: {
     rollupOptions: {
       input: {
-        index: './index.html',
-        products: './products.html', 
+        index: 'index.html',
+        products: 'products.html',
       },
     },
   },
